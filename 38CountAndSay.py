0d496120bf0bd17300068e0a589881961a95579b
NNQA <anhla12h@gmail.com>
2026-08-18 16:46:54 +0700

    add optimize the solution

--- 38CountAndSay.py
+++ 38CountAndSay.py
@@ -1,37 +1,67 @@
 from collections import Counter
 
+# orginal solution
+# class Solution:
+#     def countAndSay(self, n: int) -> str:
+
+#         def group_consecutive_digits(groups: list, s: str):
+#             previous_digit = s[0]
+#             count = 0
+
+#             for digit in s:
+#                 if digit != previous_digit:
+#                     groups.append([int(previous_digit), count])
+#                     previous_digit = digit
+#                     count = 1
+#                 else:
+#                     count += 1
+
+#             groups.append([int(previous_digit), count])
+
+#         def build_next_sequence(groups: list) -> str:
+#             return "".join(str(count) + str(digit) for digit, count in groups)
+
+#         sequence = ""
+
+#         for i in range(n):
+#             groups = []
+
+#             if i == 0:
+#                 sequence = "1"
+#                 continue
+
+#             group_consecutive_digits(groups, sequence)
+#             sequence = build_next_sequence(groups)
+
+#         return sequence
+
+
+# optimize solution
+
 
 class Solution:
     def countAndSay(self, n: int) -> str:
+        sequence = "1"
 
-        def group_consecutive_digits(groups: list, s: str):
-            previous_digit = s[0]
+        for _ in range(n - 1):
+            result = []
+            previous_digit = sequence[0]
             count = 0
 
-            for digit in s:
-                if digit != previous_digit:
-                    groups.append([int(previous_digit), count])
+            for digit in sequence:
+                if digit == previous_digit:
+                    count += 1
+                else:
+                    result.append(str(count))
+                    result.append(previous_digit)
+
                     previous_digit = digit
                     count = 1
-                else:
-                    count += 1
 
-            groups.append([int(previous_digit), count])
+            result.append(str(count))
+            result.append(previous_digit)
 
-        def build_next_sequence(groups: list) -> str:
-            return "".join(str(count) + str(digit) for digit, count in groups)
-
-        sequence = ""
-
-        for i in range(n):
-            groups = []
-
-            if i == 0:
-                sequence = "1"
-                continue
-
-            group_consecutive_digits(groups, sequence)
-            sequence = build_next_sequence(groups)
+            sequence = "".join(result)
 
         return sequence
 
