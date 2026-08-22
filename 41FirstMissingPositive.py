07e4235c8e59c0b7bf4424bb76cbd946ff52ed93
NNQA <anhla12h@gmail.com>
2026-08-22 12:45:49 +0700

    add first missing positive solution

---
+++ 41FirstMissingPositive.py
@@ -0,0 +1,17 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums = [n for n in nums if n > 0]
+        nums.sort()
+
+        target = 1
+        for n in nums:
+            if n == target:
+                target += 1
+            elif n > target:
+                return target
+
+        return target
+
+
+sol = Solution()
+print(sol.firstMissingPositive([3, 1, 2]))
