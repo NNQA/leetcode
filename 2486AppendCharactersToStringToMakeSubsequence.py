347fb8beb12edbf66d910bff01532ea0de66fb9e
NNQA <anhla12h@gmail.com>
2026-09-02 15:01:28 +0700

    add Append characters to string to make subsequence

---
+++ 2486AppendCharactersToStringToMakeSubsequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    # dp
+    def appendCharacters(self, s: str, t: str) -> int:
+        j = 0
+        for char in s:
+            if j < len(t) and char == t[j]:
+                j += 1
+        return len(t) - j
+
+    # two pointers
+    def appendCharactersTowPointers(self, s: str, t: str) -> int:
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                j += 1
+            i += 1
+        return len(t) - j
